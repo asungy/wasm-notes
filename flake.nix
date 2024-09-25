eb61909776365a82b0af22ad9c43f35c13613770
asungy <62207329+asungy@users.noreply.github.com>
2024-09-24 23:48:23 -0400

    Initialized mdbook with github workflow

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs: with inputs;
+    utils.lib.eachDefaultSystem(system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            mdbook
+            wabt
+            wasmtime
+          ];
+        };
+      }
+    );
+}
